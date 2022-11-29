5e8c8aa8c768ffa77df396a039980e67e170a14c
vo1dzin <matheus342conrado@gmail.com>
2022-11-29 08:40:26 -0300

    update

--- JogadorGoleiro.cpp
+++ JogadorGoleiro.cpp
@@ -1,5 +1,5 @@
 #include "JogadorGoleiro.h"
-
+//Construtor
 JogadorGoleiro::JogadorGoleiro(std::string nome, int idade, int habilidade, int gols, int camisa,int reflexo, int altura) : Jogador(nome, idade, habilidade, gols, camisa){
     this->reflexo = reflexo;
     this->altura = altura;
@@ -10,6 +10,7 @@ int JogadorGoleiro::getReflexo(){
 int JogadorGoleiro::getAltura(){
     return this->altura;
 }
+//Faz o calculo especifico da habilidade do jogador goleiro;
 int JogadorGoleiro::getHabilidade(){
     return (habilidade*5) + (((int)(altura*100))*2) + (reflexo*3)/10;
 }
